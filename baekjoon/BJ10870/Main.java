05c1c1dabd083e4470e74273d37a6bf8acd176f7
jujuwon <webmon@kakao.com>
2022-08-20 09:17:30 +0900

    feat: Add today's problem BJ10870

---
+++ baekjoon/BJ10870/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] fibo = new int[21];
+		fibo[0] = 0;
+		fibo[1] = 1;
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 2; i <= n; i++) {
+			fibo[i] = fibo[i - 2] + fibo[i - 1];
+		}
+
+		System.out.println(fibo[n]);
+	}
+}
