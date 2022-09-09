4a1f5b23cc4f55f8f4d2a7abc3d16b0ec2a5360a
jujuwon <webmon@kakao.com>
2022-09-09 23:01:23 +0900

    refactor: add timeToSec function

--- programmers/KAKAO 기출/PRO92341/PRO92341.py
+++ programmers/KAKAO 기출/PRO92341/PRO92341.py
@@ -1,7 +1,10 @@
 # programmers 주차 요금 계산 (Lv2 2022 KAKAO BLIND RECRUITMENT)
-from datetime import datetime
 import math
 
+def timeToSec(time):
+    a, b = time.split(":")
+    return int(a)*60 + int(b)
+    
 def solution(fees, records):
     answer = []
     
@@ -9,26 +12,25 @@ def solution(fees, records):
     result = {}
     
     for record in records:
-        r = record.split()
-        time = r[0]
-        carNum = r[1]
-        history = r[2]
+        time, carNum, history = record.split()
 
         if history == "IN" : #입차
             park[carNum] = time
             if carNum not in result.keys():
                 result[carNum] = 0
         else : #출차
-            outTime = datetime.strptime(time, "%H:%M")
-            inTime = datetime.strptime(park.pop(carNum), "%H:%M")
+            # outTime = datetime.strptime(time, "%H:%M")
+            # inTime = datetime.strptime(park.pop(carNum), "%H:%M")
             
-            result[carNum] += int((outTime - inTime).seconds / 60)
+            # result[carNum] += int((outTime - inTime).seconds / 60)
+            result[carNum] += timeToSec(time) - timeToSec(park.pop(carNum))
     
     # 만약 주차장에 입차기록만 있고 출차가 없다면
     for carNum in park.keys():
-        outTime = datetime.strptime("23:59", "%H:%M")
-        inTime = datetime.strptime(park[carNum], "%H:%M")
-        result[carNum] += int((outTime - inTime).seconds / 60)
+        # outTime = datetime.strptime("23:59", "%H:%M")
+        # inTime = datetime.strptime(park[carNum], "%H:%M")
+        # result[carNum] += int((outTime - inTime).seconds / 60)
+        result[carNum] += timeToSec("23:59") - timeToSec(park[carNum])
         
     # fees[1] + Math.올림((result) - fees[0]) / fees[2]) * fees[3]
     sorted_dict = sorted(result.items())
