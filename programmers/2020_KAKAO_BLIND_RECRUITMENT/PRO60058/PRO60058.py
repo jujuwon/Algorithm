00639a8406f253e6e9b64e03f243134219519107
jujuwon <webmon@kakao.com>
2022-09-14 22:31:48 +0900

    feat: add today's problem PRO60058

--- programmers/2020_KAKAO_BLIND_RECRUITMENT/PRO60058/PRO60058.py
+++ programmers/2020_KAKAO_BLIND_RECRUITMENT/PRO60058/PRO60058.py
@@ -1,62 +1,53 @@
 # programmers 괄호 변환 (Lv2 2020 KAKAO BLIND RECRUITMENT)
-from collections import deque
-
-def separate(p):
-    left = 0
-    right = 0
+       
+def balanced_index(p):
+    count = 0 # 왼쪽 괄호의 개수
     for i in range(len(p)):
-        if p[i] == "(":
-            left += 1
+        if p[i] == '(':
+            count += 1
         else:
-            right += 1
-        if left == right:
-            return p[:i+1], p[i+1:]
+            count -= 1
+        if count == 0:
+            return i
         
-def check(str):
-    stack = deque()
-    for s in str:
-        if s == "(":
-            stack.append(s)
+def check_proper(p):
+    count = 0
+    for i in p:
+        if i == '(':
+            count += 1
         else:
-            if not stack:
+            if count == 0:
                 return False
-            else:
-                stack.pop()
-    
-    if stack:
-        return False
+            count -= 1
     return True
-
-
+    
 def solution(p):
     answer = ''
     
     # 빈 문자열
-    if len(p) == 0:
-        return p
+    if p == '':
+        return answer
     
     # 문자열을 두 "균형잡힌 괄호 문자열" 로 분리
-    u, v = separate(p)
+    index = balanced_index(p)
+    u = p[:index+1]
+    v = p[index+1:]
     
     # 문자열 u가 올바른 괄호 문자열이라면 v 에 대해 1번부터 다시 수행
-    if check(u):
-        answer += u
-        answer += solution(v)
+    if check_proper(u):
+        answer = u + solution(v)
     # 문자열 u가 올바른 괄호 문자열이 아니라면 아래 과정 수행
     else:
-        #  빈 문자열에 첫 번째 문자로 '('를 붙입니다. 
-        #  문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
-        #  ')'를 다시 붙입니다. 
-        #  u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
-        #  생성된 문자열을 반환합니다.
         answer += '('
         answer += solution(v)
         answer += ')'
+        u = list(u[1:-1])
         
-        for s in u[1:len(u)-1]:
-            if s == '(':
-                answer += ')'
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] += ')'
             else :
-                answer += '('
+                u[i] += '('
+        answer += "".join(u)
         
     return answer
