71836ad248bd058fe7a5cf5e12ed5ec9a03da664
jujuwon <webmon@kakao.com>
2023-05-10 23:39:57 +0900

    feat: Add today's problem BJ11399

--- baekjoon/BJ11399/BJ11399.py
+++ baekjoon/BJ11399/BJ11399.py
@@ -4,12 +4,27 @@ import sys
 def input():
     return sys.stdin.readline().rstrip()
 
-N = int(input())
-P = sorted(list(map(int, input().split())))
-time = 0
+# 첫번째 풀이
+def solution_1():
+    N = int(input())
+    P = sorted(list(map(int, input().split())))
+    time = 0
 
-for i in range(1, N+1):
-    for j in range(i):
-        time += P[j]
-        
-print(time)
+    for i in range(1, N+1):
+        for j in range(i):
+            time += P[j]
+            
+    print(time)
+    
+# 두번째 풀이
+def solution_2():
+    N = int(input())
+    P = sorted(list(map(int, input().split())))
+    sum = [0 for _ in range(N)]
+    sum[0] = P[0]
+    time = sum[0]
+    
+    for i in range(1, N):
+        sum[i] = sum[i-1] + P[i]
+        time += sum[i]
+    print(time)
