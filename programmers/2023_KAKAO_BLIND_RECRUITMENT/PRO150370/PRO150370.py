d3d289345b8b68a9b00f8d241081a9b5be2d8779
jujuwon <webmon@kakao.com>
2023-06-28 21:14:08 +0900

    feat: Add today's problems PRO150370

--- programmers/2023_KAKAO_BLIND_RECRUITMENT/PRO150370/PRO150370.py
+++ programmers/2023_KAKAO_BLIND_RECRUITMENT/PRO150370/PRO150370.py
@@ -1,3 +1,4 @@
+# 예전 풀이
 def timeToDay(day_string):
     # string 들어오면 시간으로 바꾸기
     # 2000.01.02 -> 일수
@@ -21,3 +22,25 @@ def solution(today, terms, privacies):
             answer.append(idx+1)
     
     return sorted(answer)
+
+# 2023.06.28 풀이
+def timeToDay(day):
+    y, m, d = day.split('.')
+    return int(y) * 12 * 28 + int(m) * 28 + int(d)
+    
+
+def solution(today, terms, privacies):
+    answer = []
+    term = {}
+    today = timeToDay(today)
+    
+    for t in terms:
+        x, y = t.split()
+        term[x] = int(y)
+    
+    for idx, p in enumerate(privacies):
+        start_day, kind = p.split()
+        if timeToDay(start_day) + term[kind] * 28 <= today:
+            answer.append(idx+1)
+    
+    return answer
